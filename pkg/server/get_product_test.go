e456686c1965d247ea72de811b9f74d845cc9d0a
Ari Handler <ari.handler@thoughtworks.com>
2023-05-10 08:57:28 +0200

    E2E testing for the GET and POST endpoints

--- pkg/server/get_product_test.go
+++ pkg/server/get_product_test.go
@@ -1,6 +1,10 @@
-package server_test
+package server
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/go-playground/assert/v2"
 	"net/http"
 	"net/http/httptest"
@@ -8,12 +12,89 @@ import (
 )
 
 func TestGetProduct(t *testing.T) {
-	router := SetupRoutes()
+	type args struct {
+		url     string
+		method  string
+		inStore []map[string]any
+	}
 
+	tests := []struct {
+		name      string
+		args      args
+		code      int
+		isProduct bool
+		res       map[string]any
+	}{
+		{
+			name: "GET an existing product returns it",
+			args: args{
+				url:    "/product/id1",
+				method: "GET",
+				inStore: []map[string]any{
+					{
+						"id":          "id1",
+						"price":       1.0,
+						"description": "valid description",
+					},
+				},
+			},
+			code:      200,
+			isProduct: true,
+			res: map[string]any{
+				"ID":          "id1",
+				"Price":       1.0,
+				"Description": "valid description",
+			},
+		},
+		{
+			name: "GET an inexistent products returns not found",
+			args: args{
+				url:    "/product/id1",
+				method: "GET",
+			},
+			code:      404,
+			isProduct: false,
+			res: map[string]any{
+				"error": "not found",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			router := SetupRoutes()
+
+			populateStore(tt.args.inStore, router)
+
+			w := httptest.NewRecorder()
+			req, _ := http.NewRequest(tt.args.method, tt.args.url, nil)
+			router.ServeHTTP(w, req)
+
+			assert.Equal(t, tt.code, w.Code)
+			var actual map[string]any
+			err := json.Unmarshal(w.Body.Bytes(), &actual)
+			if err != nil {
+				t.Errorf("Unexpected error during unmarshaling of the response: %v", err)
+			}
+			if tt.isProduct {
+				assert.Equal(t, true, compareProductResponses(actual, tt.res))
+			} else {
+				assert.Equal(t, actual, tt.res)
+			}
+
+		})
+	}
+}
+
+func populateStore(store []map[string]any, router *gin.Engine) {
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest("GET", "/product/id1", nil)
-	router.ServeHTTP(w, req)
-
-	assert.Equal(t, 200, w.Code)
-	assert.Equal(t, "pong", w.Body.String())
+	for _, p := range store {
+		payload, _ := json.Marshal(map[string]any{
+			"price":       p["price"],
+			"description": p["description"],
+		})
+		url := fmt.Sprintf("%v%v", "/product/", p["id"])
+		req, _ := http.NewRequest("POST", url, bytes.NewReader(payload))
+		router.ServeHTTP(w, req)
+	}
 }
--- pkg/server/post_product_test.go
+++ pkg/server/post_product_test.go
@@ -10,20 +10,95 @@ import (
 )
 
 func TestPostProduct(t *testing.T) {
-	router := SetupRoutes()
+	type args struct {
+		url     string
+		method  string
+		payload map[string]any
+	}
 
-	w := httptest.NewRecorder()
-	payload, _ := json.Marshal(map[string]any{
-		"price":       1.0,
-		"description": "valid description",
-	})
-	req, _ := http.NewRequest(
-		"POST",
-		"/product/id1",
-		bytes.NewReader(payload),
-	)
-	router.ServeHTTP(w, req)
+	tests := []struct {
+		name      string
+		args      args
+		code      int
+		isProduct bool
+		res       map[string]any
+	}{
+		{
+			name: "Valid POST returns the product",
+			args: args{
+				url:    "/product/id1",
+				method: "POST",
+				payload: map[string]any{
+					"price":       1.0,
+					"description": "valid description",
+				},
+			},
+			code:      200,
+			isProduct: true,
+			res: map[string]any{
+				"ID":          "id1",
+				"Price":       1.0,
+				"Description": "valid description",
+			},
+		},
+		{
+			name: "Returns an error when product posted description is more than 50 char long",
+			args: args{
+				url:    "/product/id1",
+				method: "POST",
+				payload: map[string]any{
+					"price":       1.0,
+					"description": "invalid description because the description is over fifty characters",
+				},
+			},
+			code:      500,
+			isProduct: false,
+			res: map[string]any{
+				"error": "description should be less than 50 characters long",
+			},
+		},
+		{
+			name: "Returns an error when ID has non-ASCII characters",
+			args: args{
+				url:    "/product/id日本",
+				method: "POST",
+				payload: map[string]any{
+					"price":       1.0,
+					"description": "valid description",
+				},
+			},
+			code:      500,
+			isProduct: false,
+			res: map[string]any{
+				"error": "ID should contain only ASCII characters",
+			},
+		},
+	}
 
-	assert.Equal(t, 200, w.Code)
-	//assert.Equal(t, "pong", w.Body.String())
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			router := SetupRoutes()
+
+			w := httptest.NewRecorder()
+			payload, _ := json.Marshal(tt.args.payload)
+			req, _ := http.NewRequest(tt.args.method, tt.args.url, bytes.NewReader(payload))
+			router.ServeHTTP(w, req)
+
+			assert.Equal(t, tt.code, w.Code)
+			var actual map[string]any
+			err := json.Unmarshal(w.Body.Bytes(), &actual)
+			if err != nil {
+				t.Errorf("Unexpected error during unmarshaling of the response: %v", err)
+			}
+			if tt.isProduct {
+				assert.Equal(t, true, compareProductResponses(actual, tt.res))
+			} else {
+				assert.Equal(t, actual, tt.res)
+			}
+		})
+	}
+}
+
+func compareProductResponses(p1, p2 map[string]any) bool {
+	return p1["ID"] == p2["ID"] && p1["Price"] == p2["Price"] && p1["Description"] == p2["Description"]
 }
